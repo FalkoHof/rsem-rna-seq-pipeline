59e3158549c1e6f7ec2f7812b8ce47e0db41dbb4
Falko Hofmann <falkohofmann@gmail.com>
2016-06-13 20:40:00 +0200

    now lists full folders withouth file names

--- make_pbs_mapping_table.sh
+++ make_pbs_mapping_table.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 input_dir=$1 #uses folder supplied by command line args
 x=1
-find $input_dir -type f -name "*.bam" | while read line
+for d in $(readlink -m $input_dir/*/) ;
 do
-  b=$(basename $line)
-  y=${b%.bam}
-  printf "%d %s\n" $x $y
+  printf "%d %s\n" $x $d
   ((x++))
 done
