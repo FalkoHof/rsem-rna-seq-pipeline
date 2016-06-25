f02c48fc8f6523231992b48e68031802a0600853
falko.hofmann <falkohofmann@gmail.com>
2016-06-25 15:25:23 +0000

    code refactoring

--- rsem_make_reference.sh
+++ rsem_make_reference.sh
@@ -4,16 +4,16 @@
 #PBS -J 1
 #PBS -j oe
 #PBS -q workq
-#PBS -o /lustre/scratch/users/falko.hofmann/log/160615/160615_make_rsem_reference.log
+#PBS -o /lustre/scratch/users/falko.hofmann/log/160625/160625_make_rsem_reference.log
 #PBS -l walltime=1:00:00
 #PBS -l select=1:ncpus=8:mem=64gb
 
 # === begin ENVIRONMENT SETUP ===
 ##### specify folders and variables #####
+aligner="star"
 annotation_file=/lustre/scratch/users/$USER/Ath_annotations/nod_v01/Arabidopsis_thaliana.TAIR10.30.nod_v01.gtf
 fasta_file=/lustre/scratch/users/$USER/indices/fasta/Col_mS.fa
-out_dir=/lustre/scratch/users/falko.hofmann/indices/rsem/nod_v01
-aligner="star"
+out_dir=/lustre/scratch/users/falko.hofmann/indices/rsem/$aligner/nod_v01
 
 ##### load required modules #####
 module load RSEM/1.2.29-foss-2015a
@@ -38,7 +38,9 @@ echo 'Fasta file: ' $fasta_file
 echo 'Output directory: ' $out_dir
 echo 'Aligner to be used: ' $aligner
 
-rsem-prepare-reference --num-threads 8 --gtf $gft_file --$aligner \
+mkdir -p $out_dir
+
+rsem-prepare-reference --num-threads 8 --gtf $annotation_file --$aligner \
   $fasta_file $out_dir
 
 echo 'Building rsem reference... - Done'
